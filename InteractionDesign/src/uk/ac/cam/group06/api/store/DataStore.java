9ab1db629cb18e752c84e556d78d684d7754606e
Robtova <robtovasoft@gmail.com>
2018-05-15 14:23:19 +0100

    Create DataStore.java

---
+++ InteractionDesign/src/uk/ac/cam/group06/api/store/DataStore.java
@@ -0,0 +1,59 @@
+package uk.ac.cam.group06.api.store;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import uk.ac.cam.group06.api.API;
+import uk.ac.cam.group06.api.HourlyLocationInformation;
+import uk.ac.cam.group06.api.LocationInformation;
+
+public class DataStore {
+	
+	private static Map<String, HourlyLocationInformation> mHourlyInfo = new HashMap<String, HourlyLocationInformation>();
+
+	private static Map<String, LocationInformation> mCurrentInfo = new HashMap<String, LocationInformation>();
+	
+	/**
+	 * Returns the current weather information for the city specified. 
+	 * If not already loaded into memory the data is loaded from the API.
+	 * 
+	 * @param city - the city for which data is wanted
+	 * @param countryCode - the country code for the location
+	 * @return LocationInformation object with current weather info
+	 */
+	public static LocationInformation getCurrentInformation(String city, String countryCode) {
+		if(!mCurrentInfo.containsKey(city+countryCode)) {
+			try {
+				mCurrentInfo.put(city+countryCode, API.getCurrentInformation(city, countryCode));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				return getCurrentInformation("cambridge", "uk");
+			}
+		}
+		return mCurrentInfo.get(city+countryCode);
+	}
+	
+	/**
+	 * Returns the five day forecast for the city specified. 
+	 * If not already loaded into memory the data is loaded from the API.
+	 * 
+	 * @param city - the city for which data is wanted
+	 * @param countryCode - the country code for the location
+	 * @return HourlyLocationInformation object with five day forecast information
+	 */
+	public static HourlyLocationInformation getFiveDayForecast(String city, String countryCode) {
+		if(!mHourlyInfo.containsKey(city+countryCode)) {
+			try {
+				mHourlyInfo.put(city+countryCode, API.getFiveDayForecast(city, countryCode));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				return getFiveDayForecast("cambridge", "uk");
+			}
+		}
+		return mHourlyInfo.get(city+countryCode);
+	}
+}
