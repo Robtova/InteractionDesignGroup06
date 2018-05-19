4ccc8b08698927d90cdfb2924a48bb86ee489713
Jonas <jonas.fiala.cardiff@gmail.com>
2018-05-19 14:32:25 +0100

    Started work on settings screen

--- InteractionDesign/src/uk/ac/cam/group06/idesign/SettingsScreen.java
+++ InteractionDesign/src/uk/ac/cam/group06/idesign/SettingsScreen.java
@@ -1,40 +1,56 @@
 package uk.ac.cam.group06.idesign;
 
-//import sun.security.ssl.Debug;
 import uk.ac.cam.relf2.idesign.components.*;
+import uk.ac.cam.relf2.idesign.components.TextComponent;
 
 import java.awt.*;
 
-public class SettingsScreen extends StackComponent {
-
-    public static SettingsScreen singleton;
-
-    private GraphicComponent mHomeScreen;
-
-    private Input input;
-
-    private static Image SETTINGS = Utils.loadImage("/settings_button.png");
+public class SettingsScreen extends GraphicComponent implements ComponentListener {
+    private static Image UP_ARROW_IMAGE = Utils.loadImage("/up_arrow.png");
 
     public SettingsScreen() {
-        this.mHomeScreen = ApplicationFrame.getHomeScreen();
-        initialise();
+        setBackgroundColor(new Color(220, 220, 220, 255));
+        setSize(100, 100, false);
 
-        this.input = ApplicationFrame.getInput();
-/*
-        ic = new ImageComponent(SETTINGS);
-        ic.setSize(100, 100, false);
-        ic.setPosition(0, 50, false);
-        this.addComponent(ic);*/
+        TextComponent text = new TextComponent();
+        text.setBackgroundColor(new Color(180, 180, 180));
+        text.setFont(new Font("Ariel", Font.PLAIN, 60));
+        text.setText("Settings");
+        text.setPosition(190, 55, true);
+        addComponent(text);
+
+        ImageComponent mArrow = new ImageComponent(UP_ARROW_IMAGE);
+        //mArrow.setBorder(GraphicComponent.SCREEN_RIGHT, GraphicComponent.SCREEN_TOP);
+        mArrow.setSize(35, 35);
+        mArrow.setPosition(15, 45,  true);
+        //mArrow.setOrigin(GraphicComponent.MIDDLE_LEFT);
+        mArrow.setRotation(3*Math.PI/2);
+        GraphicComponent toDisable = this;
+        mArrow.setComponentListener(new ComponentListener() {
+            @Override
+            public void onClicked(int x, int y) {
+                ApplicationFrame.removeComponent(toDisable);
+            }
+        });
+        addComponent(mArrow);
     }
-    ImageComponent ic;
+
     @Override
-    public void update() {
-        //ic.setSize(50, 50, false);
-
+    protected void addedToComponent(GraphicComponent parent) {
+        super.addedToComponent(parent);
+        ApplicationFrame.getHomeScreen().setClickable(false);
+        ApplicationFrame.getBreakdownScreen().setClickable(false);
     }
 
-    private void initialise() {
-        this.setBackgroundColor(new Color(250, 250, 250));
-        this.setSize(100, 100, false);
+    @Override
+    protected void removedFromComponent(GraphicComponent parent) {
+        super.removedFromComponent(parent);
+        ApplicationFrame.getHomeScreen().setClickable(true);
+        ApplicationFrame.getBreakdownScreen().setClickable(true);
+    }
+
+    @Override
+    public void onClicked(int x, int y) {
+        ApplicationFrame.removeComponent(this);
     }
 }
