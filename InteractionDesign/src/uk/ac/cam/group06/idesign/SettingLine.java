3fe48dc7ca6a3be9c0bf727ce6df4e3c66808904
Jonas <jonas.fiala.cardiff@gmail.com>
2018-05-20 13:05:45 +0100

    Settings functional, if not that pretty (They dont actually toggle anything yet though). Will try to invent a few more as well.

---
+++ InteractionDesign/src/uk/ac/cam/group06/idesign/SettingLine.java
@@ -0,0 +1,63 @@
+package uk.ac.cam.group06.idesign;
+
+import uk.ac.cam.relf2.idesign.components.ComponentListener;
+import uk.ac.cam.relf2.idesign.components.GraphicComponent;
+import uk.ac.cam.relf2.idesign.components.TextComponent;
+
+import java.awt.*;
+
+public class SettingLine extends GraphicComponent implements ComponentListener{
+    private static final int mHeight = 100;
+    private static final Font mMainFont = new Font("Ariel", Font.PLAIN, 36),
+                              mBabyFont = new Font("Ariel", Font.PLAIN, 16);
+    private static final Color mBackgroundCol = new Color(250, 250, 250),
+                               mMainColor = new Color(140, 140, 140),
+                               mBabyColor = new Color(180, 180, 180);
+
+    public SettingLine(String settingText, String settingDescription){
+        setSize(100, false, mHeight, true);
+        setBackgroundColor(mBackgroundCol);
+
+        GraphicComponent divide = new GraphicComponent();
+        divide.setSize(100, false, 1, true);
+        divide.setPosition(0, mHeight-1);
+        divide.setBackgroundColor(new Color(180, 180, 180));
+        addComponent(divide);
+
+        TextComponent mainSettingText = new TextComponent();
+        mainSettingText.setAlign(TextComponent.RIGHT);
+        mainSettingText.setBackgroundColor(mMainColor);
+        mainSettingText.setFont(mMainFont);
+        mainSettingText.setText(settingText);
+        mainSettingText.setPosition(20, 33, true);
+        addComponent(mainSettingText);
+
+        TextComponent descText = new TextComponent();
+        descText.setAlign(TextComponent.RIGHT);
+        descText.setBackgroundColor(mBabyColor);
+        descText.setFont(mBabyFont);
+        descText.setText(settingDescription);
+        descText.setPosition(20, 65, true);
+        addComponent(descText);
+
+        toggle = new GraphicComponent();
+        toggle.setBackgroundColor(mOn);
+        toggle.setSize(20, 20, true);
+        toggle.setPosition(85, 50, false);
+        addComponent(toggle);
+    }
+
+    private static Color mOn = new Color(70, 70, 70),
+                         mOff = new Color(180, 180, 180);
+    private GraphicComponent toggle;
+    public void toggleGraphic() {
+        if(toggle.getBackgroundColor() == mOn)
+            toggle.setBackgroundColor(mOff);
+        else
+            toggle.setBackgroundColor(mOn);
+    }
+
+    @Override
+    public void onClicked(int x, int y) {
+    }
+}
--- InteractionDesign/src/uk/ac/cam/group06/idesign/SettingsScreen.java
+++ InteractionDesign/src/uk/ac/cam/group06/idesign/SettingsScreen.java
@@ -5,25 +5,23 @@ import uk.ac.cam.relf2.idesign.components.TextComponent;
 
 import java.awt.*;
 
-public class SettingsScreen extends GraphicComponent implements ComponentListener {
+public class SettingsScreen extends GraphicComponent {
     private static Image UP_ARROW_IMAGE = Utils.loadImage("/up_arrow.png");
 
     public SettingsScreen() {
-        setBackgroundColor(new Color(220, 220, 220, 255));
+        setBackgroundColor(new Color(250, 250, 250, 255));
         setSize(100, 100, false);
 
         TextComponent text = new TextComponent();
-        text.setBackgroundColor(new Color(180, 180, 180));
-        text.setFont(new Font("Ariel", Font.PLAIN, 60));
-        text.setText("Settings");
-        text.setPosition(190, 55, true);
+        text.setBackgroundColor(new Color(70, 70, 70));
+        text.setFont(new Font("Ariel", Font.PLAIN, 36));
+        text.setText("SETTINGS");
+        text.setPosition(150, 55, true);
         addComponent(text);
 
         ImageComponent mArrow = new ImageComponent(UP_ARROW_IMAGE);
-        //mArrow.setBorder(GraphicComponent.SCREEN_RIGHT, GraphicComponent.SCREEN_TOP);
-        mArrow.setSize(35, 35);
-        mArrow.setPosition(15, 45,  true);
-        //mArrow.setOrigin(GraphicComponent.MIDDLE_LEFT);
+        mArrow.setSize(25, 25);
+        mArrow.setPosition(15, 47,  true);
         mArrow.setRotation(3*Math.PI/2);
         GraphicComponent toDisable = this;
         mArrow.setComponentListener(new ComponentListener() {
@@ -33,6 +31,44 @@ public class SettingsScreen extends GraphicComponent implements ComponentListene
             }
         });
         addComponent(mArrow);
+
+        GraphicComponent divide = new GraphicComponent();
+        divide.setSize(100, false, 1, true);
+        divide.setPosition(0, 111);
+        divide.setBackgroundColor(new Color(180, 180, 180));
+        addComponent(divide);
+
+        StackComponent settingsList = new StackComponent();
+        settingsList.setSize(100, 86, false);
+        settingsList.setPosition(0, 112);
+        addComponent(settingsList);
+
+        SettingLine asthmatic = new SettingLine("Asthma sufferer", "Also use pollutants only hazardous to asthmatics");
+        asthmatic.setComponentListener(new ComponentListener() {
+            @Override
+            public void onClicked(int x, int y) {
+                asthmatic.toggleGraphic();
+            }
+        });
+        settingsList.addComponent(asthmatic);
+
+        SettingLine units = new SettingLine("Use metric", "Select whether to use metric or imperial units");
+        units.setComponentListener(new ComponentListener() {
+            @Override
+            public void onClicked(int x, int y) {
+                units.toggleGraphic();
+            }
+        });
+        settingsList.addComponent(units);
+
+        SettingLine notifications = new SettingLine("Display notifications", "If conditions are hazardous, display a notification?");
+        notifications.setComponentListener(new ComponentListener() {
+            @Override
+            public void onClicked(int x, int y) {
+                notifications.toggleGraphic();
+            }
+        });
+        settingsList.addComponent(notifications);
     }
 
     @Override
@@ -47,10 +83,5 @@ public class SettingsScreen extends GraphicComponent implements ComponentListene
         super.removedFromComponent(parent);
         ApplicationFrame.getHomeScreen().setClickable(true);
         ApplicationFrame.getBreakdownScreen().setClickable(true);
-    }
-
-    @Override
-    public void onClicked(int x, int y) {
-        ApplicationFrame.removeComponent(this);
     }
 }
